c96bdf4a4d742f00802040ee305d29c75daa9821
Salvador <salvador10julianes@gmail.com>
2021-06-07 22:32:38 -0300

    Added h1 tag

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <nav className="navBar">
+            <h1>Hola Tutor!</h1>
+            <ul clasName="items">
+                <li>Hola</li>
+                <li>Buenas</li>
+                <li>Noches</li>
+            </ul>
+        </nav>
+    );
+}
